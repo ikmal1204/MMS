7cdeb0aa7fbc738de0c21538f80d9dede8a0fa60
ikmal1204 <ikmalismail1204@gmail.com>
2022-12-30 17:13:55 +0800

    Get last ID

--- insertdataPOST.php
+++ insertdataPOST.php
@@ -5,7 +5,7 @@ $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
 
-echo "Welcome" . $firstname ."<br>";
+echo "Welcome " . $firstname ." ";
 echo $lastname. "<br>";
 echo "Your email address is:". $email. "<br>"; 
 
@@ -14,10 +14,12 @@ $sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('$firstname', '$lastname', '$email')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  $last_id = $conn->insert_id;
+  echo "<br> New record created successfully. Last inserted ID is: " . $last_id;
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 
 $conn->close();
 ?>
